96ad216f501311e4bc4e9c9120d5bcc4f6794383
Remco Haszing <remcohaszing@gmail.com>
2023-04-30 12:11:44 +0200

    Add implementation
    The implementation is based on `remark-break`. It was copied, then
    modified. The following modifications have been made:
    
    - The implementation was moved from `index.js` to `lib/index.js`.
    - The remark plugin was replaced with the function `newlineToBreak`.
    - The implementation was based on
      https://github.com/remarkjs/remark-breaks/issues/9#issuecomment-1523777795.
    - Tests are based on markdown output, not html output. This uncovered a
      small bug in the implementation linked above that has been fixed.
    - Tests are not based on a unified pipeline.
    - `tape` was replaced with `node:test`.
    - Each test fixture now has its own test case. This allows test
      filtering and inspecting multiple failed tests.
    - Dependencies were updated.
    - Support was dropped for Node.js < 16.
    - The readme was updated accordingly.

---
+++ test.js
@@ -0,0 +1,134 @@
+import assert from 'node:assert/strict'
+import {test} from 'node:test'
+import {fromMarkdown} from 'mdast-util-from-markdown'
+import {newlineToBreak} from 'mdast-util-newline-to-break'
+import {toMarkdown} from 'mdast-util-to-markdown'
+
+const fixtures = [
+  {
+    in: 'This is a\nparagraph.',
+    out: 'This is a\\\nparagraph.\n',
+    name: 'no space'
+  },
+  {
+    in: 'This is a \nparagraph.',
+    out: 'This is a\\\nparagraph.\n',
+    name: 'one space'
+  },
+  {
+    in: 'This is a  \nparagraph.',
+    out: 'This is a\\\nparagraph.\n',
+    name: 'two spaces'
+  },
+  {
+    in: 'This is a   \nparagraph.',
+    out: 'This is a\\\nparagraph.\n',
+    name: 'three spaces'
+  },
+  {
+    in: 'This is a\rparagraph.',
+    out: 'This is a\\\nparagraph.\n',
+    name: 'carriage return'
+  },
+  {
+    in: 'This is a\r\nparagraph.',
+    out: 'This is a\\\nparagraph.\n',
+    name: 'carriage return + line feed'
+  },
+  {
+    in: 'After *phrasing*\nmore.',
+    out: 'After *phrasing*\\\nmore.\n',
+    name: 'after phrasing'
+  },
+  {
+    in: 'Before\n*phrasing*.',
+    out: 'Before\\\n*phrasing*.\n',
+    name: 'before phrasing'
+  },
+  {
+    in: 'Mul\nti\nple.',
+    out: 'Mul\\\nti\\\nple.\n',
+    name: 'multiple'
+  },
+  {
+    in: 'None.',
+    out: 'None.\n',
+    name: 'none'
+  },
+  {
+    in: [
+      'no space',
+      'asd',
+      '',
+      'one space ',
+      'asd',
+      '',
+      'one tab	',
+      'asd',
+      '',
+      'in an ![image',
+      'alt](#)',
+      '',
+      'in a [link',
+      'alt](#)',
+      '',
+      'in an *emphasis',
+      'emphasis*.',
+      '',
+      'in a **strong',
+      'strong**.',
+      '',
+      'setext',
+      'heading',
+      '===',
+      '',
+      '> block',
+      '> quote.',
+      '',
+      '* list',
+      '  item.'
+    ].join('\n'),
+    out: [
+      'no space\\',
+      'asd',
+      '',
+      'one space\\',
+      'asd',
+      '',
+      'one tab\\',
+      'asd',
+      '',
+      'in an ![image',
+      'alt](#)',
+      '',
+      'in a [link\\',
+      'alt](#)',
+      '',
+      'in an *emphasis\\',
+      'emphasis*.',
+      '',
+      'in a **strong\\',
+      'strong**.',
+      '',
+      'setext\\',
+      'heading',
+      '=======',
+      '',
+      '> block\\',
+      '> quote.',
+      '',
+      '*   list\\',
+      '    item.',
+      ''
+    ].join('\n'),
+    name: 'document'
+  }
+]
+
+for (const fixture of fixtures) {
+  test(fixture.name, () => {
+    const mdast = fromMarkdown(fixture.in)
+    newlineToBreak(mdast)
+    assert.equal(toMarkdown(mdast), fixture.out)
+  })
+}
